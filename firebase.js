06bcb6df0368d28de1dc826950c98acdbbff8edb
Satyala Denny <denny.16satyala@gmail.com>
2025-07-24 20:44:58 +0530

    added and upadated firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,16 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
-
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZXeRx4hcTVMkpW1Q0nZecotSqcrv-Kuo",
-  authDomain: "fir-b3428.firebaseapp.com",
-  projectId: "fir-b3428",
-  storageBucket: "fir-b3428.firebasestorage.app",
-  messagingSenderId: "411105537603",
-  appId: "1:411105537603:web:10527d6d1c70a9f77b6ef6",
-  measurementId: "G-7DD2YXSPC8",
-  databaseURL: "https://fir-b3428-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app)
-
+export const db = getDatabase(app);
